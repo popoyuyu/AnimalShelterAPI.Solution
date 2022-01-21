a850908b8d4ebc574b2ad145affd66a487ab7538
Jeff Lai <pjefflai@gmail.com>
2022-01-21 09:42:21 -0800

    add necessary models files

---
+++ AnimalShelterAPI/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelterAPI.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Description { get; set; }
+  }
+}
