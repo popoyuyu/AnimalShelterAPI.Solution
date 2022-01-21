d47aec89f6bd1c3d963ad4fa0c0790d09a30cc0a
Jeff Lai <pjefflai@gmail.com>
2022-01-21 15:04:56 -0800

    add validation parameters

--- AnimalShelterAPI/Models/Animal.cs
+++ AnimalShelterAPI/Models/Animal.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 namespace AnimalShelterAPI.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
+    [Range(1, 30, ErrorMessage = "Age must be betwen 1 and 30.")]
     public int Age { get; set; }
+    [Required]
     public string Gender { get; set; }
+    [Required]
     public string Description { get; set; }
   }
 }
