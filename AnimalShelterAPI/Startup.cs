0b341333cc2de4e07218dd6d80902e0c26f9c217
Jeff Lai <pjefflai@gmail.com>
2022-01-21 16:02:11 -0800

    add swagger to API

--- AnimalShelterAPI/Startup.cs
+++ AnimalShelterAPI/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
 using AnimalShelterAPI.Models;
 
 namespace AnimalShelterAPI
@@ -24,7 +26,31 @@ namespace AnimalShelterAPI
       services.AddDbContext<AnimalShelterAPIContext>(opt =>
           opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
+
+      //swagger
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Animal Shelter",
+          Description = "An ASP.NET Core Web API for managing Animals",
+          TermsOfService = new Uri("https://example.com/terms"),
+          Contact = new OpenApiContact
+          {
+            Name = "Jeff",
+            Url = new Uri("https://example.com/contact")
+          },
+          License = new OpenApiLicense
+          {
+            Name = "new OpenAPILicense",
+            Url = new Uri("https://example.com/license")
+          }
+        });
+      });
     }
+
+
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,6 +61,13 @@ namespace AnimalShelterAPI
       }
 
       // app.UseHttpsRedirection();
+      app.UseSwagger();
+
+      app.UseSwaggerUI(options =>
+      {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+      });
 
       app.UseRouting();
 
